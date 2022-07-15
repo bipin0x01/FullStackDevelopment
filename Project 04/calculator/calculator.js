8b47f3e1269ffb5c0576a5503f73caebeb2f7ba8
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-07-15 23:40:14 +0530

    calculator

--- Project 04/calculator/calculator.js
+++ Project 04/calculator/calculator.js
@@ -1,11 +1,13 @@
 // jshint esversion:6
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser);
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("This is homepage.")
+    res.sendFile(__dirname + '/index.js');
 });
 
 app.listen(port, () => {
